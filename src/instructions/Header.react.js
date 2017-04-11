525d3ac41bfa3be2d739bd89a98c2af2b53570a8
Luke Golden <webseedsonline@gmail.com>
2017-04-11 19:05:31 -0400

    Updated demo url link

--- src/instructions/Header.react.js
+++ src/instructions/Header.react.js
@@ -19,7 +19,7 @@ export default class Header extends Component {
               </object>
               <h1 className="project-name">CityBook</h1>
               <h2 className="project-tagline">Create a contact list from a Google Spreadsheet.</h2>
-              <a href="#/books/2a75006d-59c5-492c-ab2f-c44d79824cc5" target="_blank" className="btn btn-header btn-lg btn-block">Citybook Demo <Glyphicon glyph='new-window' /></a>
+              <a href="#/books/c48d038a-f724-4745-a428-fa950ca06c4f" target="_blank" className="btn btn-header btn-lg btn-block">Citybook Demo <Glyphicon glyph='new-window' /></a>
               <ScrollLink
                 to="instructions-start"
                 smooth={true}
@@ -31,7 +31,7 @@ export default class Header extends Component {
               <Col sm={6} smOffset={3}>
                 <span className='demo-phone-camera'></span>
                 <span className='demo-phone-speaker'></span>
-                <iframe className='demo-iframe' src="https://www.safe-reaches-21228.herokuapp.com/#/books/2a75006d-59c5-492c-ab2f-c44d79824cc5" width="100%" height="550px" frameBorder="0"></iframe>
+                <iframe className='demo-iframe' src="https://www.safe-reaches-21228.herokuapp.com/#/books/c48d038a-f724-4745-a428-fa950ca06c4f" width="100%" height="550px" frameBorder="0"></iframe>
                 <span className='demo-iframe-bottom-line'></span>
               </Col>
             </Col>
